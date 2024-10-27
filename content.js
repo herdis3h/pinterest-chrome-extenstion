97df426374fc8acb205158b79e21718462194f8b
herdis <herdis@spectrm.io>
2024-10-27 13:37:00 +0100

    stykle buttons and scroll for select

--- content.js
+++ content.js
@@ -69,16 +69,16 @@ function observeImageList() {
 
 // Stop auto-scroll and observer
 function stopAutoScrollAndObserver() {
-  console.log("scrollInterval", scrollInterval)
   if (scrollInterval) {
-    clearInterval(scrollInterval);  // Stop auto-scroll
+    clearInterval(scrollInterval);
+    scrollInterval = null;
   }
   if (observer) {
-    observer.disconnect();  // Stop observing
+    observer.disconnect();
+    observer = null;
     console.log('Stopped auto-scroll and observer.');
   }
-
-  document.body.style.overflow = ''; 
+  document.body.style.overflow = '';
 }
 
 // Fetch images from a div with role="list" and update selectedImages
@@ -111,34 +111,58 @@ function fetchImages() {
   }
 }
 
-
-// Clear selected images
 function clearSelectedImages() {
-  selectedImages = [];  // Empty the selected images array
-  updatePanel();  // Update the panel to reflect the change
+  selectedImages = [];  // Clear the selected images array
+  updatePanel();  // Update the panel to reflect the cleared selection
+
+  // Stop any active auto-scroll and observer
+  stopAutoScrollAndObserver();
+
 }
 
 
+
 // Start observing and scrolling
-function startFetchingImages() {
+async function startFetchingImages() {
+  const listDiv = document.querySelector('div[role="list"]');
+
+  if (listDiv) {
+    const rect = listDiv.getBoundingClientRect();
+
+    // Check if listDiv is out of view, adjust scroll position if needed
+    if (rect.top < 0 || rect.bottom > window.innerHeight) {
+     await window.scrollBy({ top: rect.top, behavior: 'smooth' });
+    }
+  }
+
+  // Now start observing and auto-scrolling
   observeImageList();
   autoScroll();  // Start smooth auto-scroll
 }
 
+
 // Handle the Fetch Images button click
 async function fetchImagesButtonClick() {
   const loader = document.getElementById('loader');
-  
-  // Show the loader
-  loader.style.display = 'block';
+
+  // Check if loader exists
+  if (loader) {
+    // Show the loader
+    loader.style.display = 'block';
+  } else {
+    console.warn("Loader element not found");
+  }
 
   await startFetchingImages();  // Start fetching and scrolling
 
-   // Hide the loader once the fetching process is done
-   setTimeout(() => {
-    loader.style.display = 'none';
-  }, 500); 
+  // Hide the loader once the fetching process is done
+  setTimeout(() => {
+    if (loader) {
+      loader.style.display = 'none';
+    }
+  }, 500);
 }
+
 
 
 
@@ -158,34 +182,54 @@ function injectPanel() {
   panel.style.zIndex = '10000';
 
   const header = document.createElement('h3');
-  header.textContent = 'Selected Images';
-  header.style.paddingBottom = '10px';
+  header.textContent = 'PinSaver';
+  header.style.paddingBottom = '5px';
   panel.appendChild(header);
+
+  // Description text under header
+  const description = document.createElement('span');
+  description.textContent = 'Select, export all images from your Pinterest moodboard.';
+  description.style.fontSize = '12px';
+  description.style.color = '#8d8d8d';
+  description.style.display = 'block';
+  description.style.marginBottom = '10px';
+  panel.appendChild(description);
+
+  const selectedImageList = document.createElement('div');
+  selectedImageList.id = 'selected-images-list';
+  selectedImageList.style.display = 'flex';  // Display images in a row
+  selectedImageList.style.position = 'relative';  // Display images in a row
+  selectedImageList.style.height = '150px';  // Enable horizontal scrolling
+  selectedImageList.style.overflowX = 'auto';  // Enable horizontal scrolling
+  selectedImageList.style.overflowY = 'hidden';  // Enable horizontal scrolling
+  selectedImageList.style.whiteSpace = 'nowrap';  // Prevent line breaks
+  selectedImageList.style.border = '1px solid #dbdbdb';  // Prevent line breaks
+  selectedImageList.style.borderRadius = '8px';  // Prevent line breaks
+  selectedImageList.style.padding = '5px';  // Prevent line breaks
+  selectedImageList.style.background = '#f5f5f5';  // Prevent line breaks
+  selectedImageList.style.marginBottom = '10px';  // Prevent line breaks
+
+  panel.appendChild(selectedImageList);
 
   // Loader element
   const loader = document.createElement('div');
   loader.id = 'loader';
-  loader.style.display = 'none';  // Initially hidden
+  loader.style.position = 'absolute';
+  loader.style.left = '40%';
+  loader.style.top = '35%';
   loader.style.border = '5px solid #f3f3f3';  // Outer circle
   loader.style.borderRadius = '50%';
   loader.style.borderTop = '5px solid #3498db';  // Blue colored rotating part
   loader.style.width = '30px';
   loader.style.height = '30px';
+  loader.style.display = 'none';
   loader.style.animation = 'spin 1s linear infinite';  // Animation for spinning
-  loader.style.margin = '10px auto';  // Center the loader
-  panel.appendChild(loader);
-
-  const selectedImageList = document.createElement('div');
-  selectedImageList.id = 'selected-images-list';
-  selectedImageList.style.display = 'flex';  // Display images in a row
-  selectedImageList.style.height = '150px';  // Enable horizontal scrolling
-  selectedImageList.style.overflowX = 'auto';  // Enable horizontal scrolling
-  selectedImageList.style.whiteSpace = 'nowrap';  // Prevent line breaks
-  panel.appendChild(selectedImageList);
+  selectedImageList.appendChild(loader);
 
   // Add an element to show the image count
   const imageCount = document.createElement('div');
   imageCount.id = 'image-count';
+  imageCount.textContent = `Selected Images Count: 0`;
   imageCount.style.marginBottom = '10px';
   panel.appendChild(imageCount);
 
@@ -198,36 +242,50 @@ function injectPanel() {
   panel.appendChild(buttonContainer);
   
 
-  // Download Button
-  const downloadButton = document.createElement('button');
-  downloadButton.textContent = 'Download Selected Images';
-  downloadButton.style.border = '0'; 
-  downloadButton.style.borderRadius = '20px'; 
-  downloadButton.style.padding = '15px'; 
-  downloadButton.style.width = '100%'; 
-  downloadButton.onclick = downloadSelectedImages;
-  buttonContainer.appendChild(downloadButton);
-
   // Fetch Images Button
   const fetchImagesButton = document.createElement('button');
-  fetchImagesButton.textContent = 'Fetch Images';
+  fetchImagesButton.textContent = 'Select all images in board';
   fetchImagesButton.style.border = '0'; 
   fetchImagesButton.style.borderRadius = '20px'; 
-  fetchImagesButton.style.padding = '15px'; 
+  fetchImagesButton.style.padding = '10px 15px'; 
   fetchImagesButton.style.width = '100%'; 
   fetchImagesButton.onclick = fetchImagesButtonClick;
   buttonContainer.appendChild(fetchImagesButton);
 
+  // Create a nested div for "Remove all images" and "Download" buttons
+  const actionButtonsContainer = document.createElement('div');
+  actionButtonsContainer.style.display = 'flex';
+  actionButtonsContainer.style.flexDirection = 'row-reverse';
+  actionButtonsContainer.style.gap = '10px';
+  actionButtonsContainer.style.justifyContent = 'flex-end';
+
   // Clear Images Button
   const clearButton = document.createElement('button');
-  clearButton.textContent = 'Clear Selected Images';
+  clearButton.textContent = 'Reset';
   clearButton.style.border = '0'; 
   clearButton.style.borderRadius = '20px'; 
-  clearButton.style.padding = '15px'; 
-  clearButton.style.width = '100%'; 
+  clearButton.style.padding = '10px 15px'; 
+  clearButton.style.width = 'fit-content'; 
   clearButton.onclick = clearSelectedImages;
-  buttonContainer.appendChild(clearButton);
+  actionButtonsContainer.appendChild(clearButton);
 
+  // Download Button with Icon
+  const downloadButton = document.createElement('button');
+  downloadButton.style.border = '0'; 
+  downloadButton.style.borderRadius = '20px'; 
+  downloadButton.style.padding = '10px 15px'; 
+  downloadButton.style.width = 'fit-content'; 
+  downloadButton.onclick = downloadSelectedImages;
+
+  const exportIcon = document.createElement('span');
+  exportIcon.innerHTML = `<svg style="height: 16px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 242.7-73.4-73.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L288 274.7 288 32zM64 352c-35.3 0-64 28.7-64 64l0 32c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-32c0-35.3-28.7-64-64-64l-101.5 0-45.3 45.3c-25 25-65.5 25-90.5 0L165.5 352 64 352zm368 56a24 24 0 1 1 0 48 24 24 0 1 1 0-48z"/></svg>`;
+
+  // Append the icon and text to the download button
+  downloadButton.appendChild(exportIcon);
+  actionButtonsContainer.appendChild(downloadButton);
+
+  // Append the nested div to the main button container
+  buttonContainer.appendChild(actionButtonsContainer);
 
   document.body.appendChild(panel);
 }
