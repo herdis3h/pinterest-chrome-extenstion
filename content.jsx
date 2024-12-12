c49baa4fc4bf35bd496daf1ad387ca2c2f7056e7
herdis <herdis@spectrm.io>
2024-12-12 06:10:08 +0100

    update manifest

--- content.jsx
+++ content.jsx
@@ -89,7 +89,7 @@ import { saveAs } from 'file-saver';
   }
   
  
- chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+ chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
   console.log("Received message:", message);
 
   if (message.action === "RemoveScript") {
@@ -111,9 +111,9 @@ import { saveAs } from 'file-saver';
     // // Clear selected images and observed items
     window.selectedImages.clear();
     window.selectExportImages.clear();
-    window.observedItems.clear();
+    await window.observedItems.clear();
     
-    const imageList = document.getElementById('selected-images-list');
+    const imageList = await document.getElementById('selected-images-list');
     if (imageList) {
         imageList.innerHTML = ''; // Clear all child elements
       }
@@ -607,7 +607,7 @@ function injectPanel() {
 
   const exportIcon = document.createElement('span');
   exportIcon.innerHTML = `
-    Download as . 
+    Download as .zip
     <svg style="height: 16px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 242.7-73.4-73.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L288 274.7 288 32zM64 352c-35.3 0-64 28.7-64 64l0 32c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-32c0-35.3-28.7-64-64-64l-101.5 0-45.3 45.3c-25 25-65.5 25-90.5 0L165.5 352 64 352zm368 56a24 24 0 1 1 0 48 24 24 0 1 1 0-48z"/></svg>
   `;
   
@@ -638,16 +638,7 @@ function injectPanel() {
   toggleUI();
 }
 
-function updateDownloadButtonText() {
-  const downloadButton = document.getElementById('download-button');
-  if (!downloadButton) return;
-
-  const totalImages = window.selectedImages.size;
-  const selectedImages = window.selectExportImages.size;
-
-  downloadButton.innerHTML = `Download as zip (${selectedImages}/${totalImages} selected) <svg style="height: 16px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 242.7-73.4-73.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L288 274.7 288 32zM64 352c-35.3 0-64 28.7-64 64l0 32c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-32c0-35.3-28.7-64-64-64l-101.5 0-45.3 45.3c-25 25-65.5 25-90.5 0L165.5 352 64 352zm368 56a24 24 0 1 1 0 48 24 24 0 1 1 0-48z"/></svg>`;
-}
-
+ 
 
 function toggleUI() {
   // profile-pins-grid
@@ -655,6 +646,8 @@ function toggleUI() {
   const panelContent = document.getElementById("panel-content")
   // console.log("panelContent", panelContent)
   const errorMessage = document.getElementById("error-message")
+
+  if(!errorMessage || !panelContent) return;
   // console.log("errorMessage", errorMessage)
   // console.log("profilePinsGrid", profilePinsGrid)
   if (profilePinsGrid) {
